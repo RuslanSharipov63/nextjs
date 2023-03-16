3e807e0910ca091b755723424980ef5c6b0432db
unknown <sharipov.r@mail.ru>
2023-03-17 00:58:31 +0400

    createstaticpage

--- pages/burgers/index.js
+++ pages/burgers/index.js
@@ -1,3 +1,7 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '@/styles/Burgers.module.css'
+
 export const getStaticProps = async () => {
     const res = await fetch('http://localhost:5000/items');
     const data = await res.json();
@@ -6,11 +10,35 @@ export const getStaticProps = async () => {
     }
 }
 
-const Burgers = (props) => {
+const Burgers = ({ burgers }) => {
 
     return (
         <div>
             <h1>Наши бургеры</h1>
+            {burgers.map(burger => {
+                return (
+                    <Link
+                        href={`/burgers/${burger.id}`}
+                        key={burger.id}
+                        className={styles.burgerCard}
+                    >
+                        <div className={styles.imageContainer}>
+                            <Image
+                                src={`${burger.image}`}
+                                alt={`${burger.name}`}
+                                width={100}
+                                height={100}
+                                Layout="responsive"
+                                objectFit="cover"
+                            />
+                        </div>
+                        <div>
+                            <h3>{burger.name}</h3>
+                            <p>{burger.desc}</p>
+                        </div>
+                    </Link>
+                );
+            })}
         </div>
     );
 }
